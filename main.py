39a2b056b9808e6c3352f576f6860b3bf8465504
Silas <s9956385@stud.uni-frankfurt.de>
2023-04-17 19:16:45 -0400

    int4

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from transformers import AutoTokenizer, AutoModel
 # import sentencepiece as spm
 # import torch
 # import cuda
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True)
+model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True).half().cuda()
 model = model.eval()
 response, history = model.chat(tokenizer, "Name 10 words that are as different from each other as possible.", history=[])
 print(response)
 # response, history = model.chat(tokenizer, "晚上睡不着应该怎么办", history=history)
 # print(response)
+g
