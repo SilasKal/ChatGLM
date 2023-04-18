9b4ae975c91932307dc5c09b993021f1c4febd5f
Silas <s9956385@stud.uni-frankfurt.de>
2023-04-18 00:33:56 -0400

    encoding utf8

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ def dat(prompt, filename_raw, filename2, num_responses):
 
 
 dat(disassociationprompt, 'response_100_prompt1.txt', 'response_prompt1_chatglm.tsv', 100)
-dat(disassociationprompt2, 'response_100_prompt1.txt', 'response_prompt1_chatglm.tsv', 100)
+dat(disassociationprompt2, 'response_100_prompt2.txt', 'response_prompt2_chatglm.tsv', 100)
