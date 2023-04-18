87bf17b3316f87397b2430c22ef540d4ff0cb234
Silas <s9956385@stud.uni-frankfurt.de>
2023-04-17 23:53:41 -0400

    added api

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from transformers import AutoTokenizer, AutoModel
 # import sentencepiece as spm
 # import torch
 # import cuda
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-int8", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b-int8", trust_remote_code=True).half().cuda()
-model = model.eval()
-response, history = model.chat(tokenizer, "Name 10 words that are as different from each other as possible.", history=[])
-print(response)
+# import openai
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/chatglm/<user_input>')
+def chatglm(user_input):
+    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-int8", trust_remote_code=True)
+    model = AutoModel.from_pretrained("THUDM/chatglm-6b-int8", trust_remote_code=True).half().cuda()
+    model = model.eval()
+    response, history = model.chat(tokenizer, user_input, history=[])
+    print(response)
+    return response
+
+app.run()
 # response, history = model.chat(tokenizer, "晚上睡不着应该怎么办", history=history)
 # print(response)
+
