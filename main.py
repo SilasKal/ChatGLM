68c0322b31d65c658b58e71d7b1cf0f763e9cec0
Silas <s9956385@stud.uni-frankfurt.de>
2023-04-18 00:06:29 -0400

    external acess to api

--- main.py
+++ main.py
@@ -14,8 +14,14 @@ def chatglm(user_input):
     response, history = model.chat(tokenizer, user_input, history=[])
     print(response)
     return response
-
-app.run()
+app.run(host='0.0.0.0')
 # response, history = model.chat(tokenizer, "晚上睡不着应该怎么办", history=history)
 # print(response)
 
+# import requests
+#
+# name = 'John'
+#
+# response = requests.get(f'http://127.0.0.0.1:5000/chatglm/hello')
+#
+# print(response.text)  # prints "Hello, John!"
